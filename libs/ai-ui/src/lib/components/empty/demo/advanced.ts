eac18c06dddd9e06ee8ece389f4720b6c9aaa501
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2026-02-08 12:41:37 -0300

    📦 refactor (empty): 📦 refactoring empty component images

--- libs/ai-ui/src/lib/components/empty/demo/advanced.ts
+++ libs/ai-ui/src/lib/components/empty/demo/advanced.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+
 import { AiAvatar, AiAvatarGroup } from "../../avatar";
 import { AiButton } from "../../button";
 import { AiEmpty } from "../empty";
@@ -10,9 +11,9 @@ import { AiEmpty } from "../empty";
             <ai-empty [image]="customImage" [title]="customTitle" description="Invite your team to collaborate on this project." [actions]="[actionInvite]">
                 <ng-template #customImage>
                     <ai-avatar-group>
-                        <ai-avatar image="./img/avatar/01.png" size="md" class="grayscale"></ai-avatar>
-                        <ai-avatar image="./img/avatar/02.jpg" size="md" class="grayscale"></ai-avatar>
-                        <ai-avatar image="./img/avatar/aiandra.jpg" size="md" class="grayscale"></ai-avatar>
+                        <ai-avatar image="https://raw.githubusercontent.com/aiandralves/smart-watch/refs/heads/main/assets/img/team/person2.jpg" size="md" class="grayscale" />
+                        <ai-avatar image="https://raw.githubusercontent.com/aiandralves/smart-watch/refs/heads/main/assets/img/team/person4.jpg" size="md" class="grayscale" />
+                        <ai-avatar image="https://avatars.githubusercontent.com/u/33138081?v=4" size="md" class="grayscale" />
                     </ai-avatar-group>
                 </ng-template>
 
