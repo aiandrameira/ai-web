ad9e6124b8880aa5597d74ac4d9b5808b1a31498
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2026-06-11 11:34:34 -0300

    🚧 chore: add selector in demo component float-button

--- libs/ai-ui/src/lib/components/float-button/demo/float-button.ts
+++ libs/ai-ui/src/lib/components/float-button/demo/float-button.ts
@@ -9,29 +9,11 @@ export const FLOAT_BUTTON = {
     componentName: "float-button",
     componentType: "float-button",
     examples: [
-        {
-            name: "variant",
-            component: DemoFloatButtonVariantComponent,
-        },
-        {
-            name: "shape",
-            component: DemoFloatButtonShapeComponent,
-        },
-        {
-            name: "size",
-            component: DemoFloatButtonSizeComponent,
-        },
-        {
-            name: "position",
-            component: DemoFloatButtonPositionComponent,
-        },
-        {
-            name: "float-group",
-            component: DemoFloatButtonGroupComponent,
-        },
-        {
-            name: "float-top",
-            component: DemoFloatButtonTopComponent,
-        },
+        { name: "variant", component: DemoFloatButtonVariantComponent },
+        { name: "shape", component: DemoFloatButtonShapeComponent },
+        { name: "size", component: DemoFloatButtonSizeComponent },
+        { name: "position", component: DemoFloatButtonPositionComponent },
+        { name: "float-group", component: DemoFloatButtonGroupComponent },
+        { name: "float-top", component: DemoFloatButtonTopComponent },
     ],
 };
