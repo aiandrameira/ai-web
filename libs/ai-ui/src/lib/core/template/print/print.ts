a8e8e271fc08407c5d7583d36131259a76fbfb80
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2026-06-12 09:15:13 -0300

    🚧 chore: add validation pipe and component print

--- libs/ai-ui/src/lib/core/template/print/print.ts
+++ libs/ai-ui/src/lib/core/template/print/print.ts
@@ -6,9 +6,11 @@ import { AiPrintPipe } from "./print.pipe";
     selector: "ai-print",
     imports: [AiPrintPipe],
     template: `
-        <div class="mt-2 bg-zinc-100 dark:bg-zinc-800 rounded py-3 pl-3 pr-5 w-full">
-            <pre class="font-code text-sm font-medium dark:bg-zinc-800" [innerHTML]="obj() | aiPrint"></pre>
-        </div>
+        @if (obj()) {
+            <div class="mt-2 bg-zinc-100 dark:bg-zinc-800 rounded py-3 pl-3 pr-5 w-full">
+                <pre class="font-code text-sm font-medium dark:bg-zinc-800" [innerHTML]="obj() | aiPrint"></pre>
+            </div>
+        }
     `,
 })
 export class AiPrint<T> {
