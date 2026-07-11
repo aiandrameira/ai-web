9302099b4e0951ed1f90eb36910cefc4274b364f
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2026-07-11 15:44:17 -0300

    🚧 chore: add icon support and improve dialog header styling

--- libs/ai-ui/src/lib/components/dialog/dialog.config.ts
+++ libs/ai-ui/src/lib/components/dialog/dialog.config.ts
@@ -1,19 +1,23 @@
 import { EventEmitter, TemplateRef, Type, ViewContainerRef } from "@angular/core";
 
 import { noopFn } from "../../core";
+import { ButtonVariants } from "../button/button.variants";
 import { AiIconType } from "../icon";
 
 export type onClickCallback<T> = (instance: T) => false | void | object;
 
 export class AiDialogConfig<T, U> {
     cancelIcon?: AiIconType;
+    cancelShape?: ButtonVariants["shape"];
     cancelText?: string | null;
     confirmIcon?: AiIconType;
+    confirmShape?: ButtonVariants["shape"];
     confirmText?: string | null;
     closable?: boolean;
     component?: string | TemplateRef<T> | Type<T>;
     customClasses?: string;
     data?: U;
+    icon?: AiIconType;
     title?: string | TemplateRef<T>;
     description?: string;
     hideFooter?: boolean;
